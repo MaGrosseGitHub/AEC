aa3662aea840383f140808945e7856fd8a8c7aad
Radouane <rad.l@live.fr>
2015-07-07 22:05:42 +0200

    Better Search css

--- core/jsIncludes.php
+++ core/jsIncludes.php
@@ -84,7 +84,8 @@
 <!-- ************************************** ************ ******************************************* -->
 <!-- ************************************** ************ ******************************************* -->
 
-    <?php echo HTML::JS("View/Layout/Default/search/uisearch"); ?>
+    <?php //echo HTML::JS("View/Layout/Default/search/uisearch"); ?>
+    <?php echo HTML::JS("View/Layout/Default/search/search"); ?>
 
 <!-- ************************************** ************ ******************************************* -->
 <!-- ************************************** ************ ******************************************* -->
@@ -99,45 +100,8 @@
 <script type="text/javascript">
     
     $(document).ready(function() {
-        new UISearch( document.getElementById( 'ct-search' ) );
+        // new UISearch( document.getElementById( 'ct-search' ) );
     });
-
-    
-    // jQuery(document).ready(function($) {
-    //   var $ctsearch = $( '#ct-search' ),
-    //     $ctsearchinput = $ctsearch.find('input.ct-search-input'),
-    //     $body = $('html,body'),
-    //     openSearch = function() {
-    //       $ctsearch.data('open',true).addClass('ct-search-open');
-    //       $ctsearchinput.focus();
-    //       return false;
-    //     },
-    //     closeSearch = function() {
-    //       $ctsearch.data('open',false).removeClass('ct-search-open');
-    //     };
-
-    //   $ctsearchinput.on('click',function(e) { e.stopPropagation(); $ctsearch.data('open',true); });
-
-    //   $ctsearch.on('click',function(e) {
-    //     e.stopPropagation();
-    //     if( !$ctsearch.data('open') ) {
-
-    //       openSearch();
-
-    //       $body.off( 'click' ).on( 'click', function(e) {
-    //         closeSearch();
-    //       } );
-
-    //     }
-    //     else {
-    //       if( $ctsearchinput.val() === '' ) {
-    //         closeSearch();
-    //         return false;
-    //       }
-    //     }
-    //   });
-
-    // });
 
     // "js/html5shiv.js"
     //"js/html5shiv-printshiv.js" 
